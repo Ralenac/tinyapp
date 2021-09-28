0f0972623d9201f26b5336a2160f8d69db8c584d
Ralenac <t.ralenac@gmail.com>
2021-09-28 17:27:51 -0400

    Updating URLs

--- express_server.js
+++ express_server.js
@@ -8,6 +8,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//CREATE
+
 function generateRandomString() {
   const randomString = Math.random().toString(36).substr(2, 6);
   return randomString;
@@ -18,7 +20,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); 
+  // console.log(req.body); 
   const shortUrl = generateRandomString(); 
   urlDatabase[shortUrl] = req.body.longURL;
   res.redirect(`/urls/${shortUrl}`);  //Redirect Short URLs   
@@ -44,8 +46,26 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+//UPDATE 
+
+app.post("/urls/:shortURL", (req, res) => {
+  const shortUrlId = req.params.shortURL;
+  const urlConted = req.body.urlConted;
+  urlDatabase[req.params.shortURL] = urlConted;
+  res.redirect("/urls")
+});
+
+app.post("/urls/:shortURL/update", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls")
 });
 
 //DELETE
@@ -56,17 +76,17 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listeniong on port ${PORT}!`);

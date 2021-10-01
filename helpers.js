13616f00608299def281371736e195675a877835
Ralenac <t.ralenac@gmail.com>
2021-10-01 00:14:53 -0400

    Adding Helper function

---
+++ helpers.js
@@ -0,0 +1,12 @@
+const findUserByEmail = function(email, users) {
+  for (let userId in users) {
+    const user = users[userId];
+    if (email === user.email) {
+      return user;
+    }
+  }
+  return undefined;
+};
+
+
+module.exports = { findUserByEmail };
